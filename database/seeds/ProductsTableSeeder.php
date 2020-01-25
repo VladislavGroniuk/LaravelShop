b2f2f4b26ad63d060aef9176eb7473be0db4c4d0
vladislavgronuk <vladislav.groniuk@gmail.com>
2020-01-25 15:28:48 +0200

    #7 Разработка интернет-магазина на Laravel 6. Seeding(посевы)

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 1; $i < 11; $i++)
+            \Illuminate\Support\Facades\DB::table('products')->insert([
+                'title' => 'Product '.$i,
+                'price' => rand(200, 1500),
+                'in_stock' => 1,
+                'description' => 'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.',
+            ]);
+    }
+}
