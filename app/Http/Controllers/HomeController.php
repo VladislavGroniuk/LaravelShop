31d5c69349c29c1efd6df919e93733a91821d29f
vladislavgronuk <vladislav.groniuk@gmail.com>
2020-02-21 12:03:56 +0200

    #8 Разработка интернет-магазина на Laravel 6. Что такое модели(models)?

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
+        $products = Product::where('id',5)->first();
+
         return view('home.index');
     }
 }
