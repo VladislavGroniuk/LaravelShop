28c122cbe5222a5e8d31feecb2de2b242fa59501
vladislavgronuk <vladislav.groniuk@gmail.com>
2020-05-14 00:48:18 +0300

    #12 Разработка интернет-магазина на Laravel 6. Вывод карточки товара. Часть 2.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -147,7 +147,12 @@
                                 <div class="product_extra product_new"><a href="categories.html">New</a></div>
                                 <div class="product_content">
                                     <div class="product_title"><a href="{{route('showProduct',['category',$product->id])}}">{{$product->title}}</a></div>
-                                    <div class="product_price">${{$product->price}}</div>
+                                    @if($product->new_price != null)
+                                        <div style="text-decoration: line-through">${{$product->price}}</div>
+                                        <div class="product_price">${{$product->new_price}}</div>
+                                    @else
+                                        <div class="product_price">${{$product->price}}</div>
+                                    @endif
                                 </div>
                             </div>
                         @endforeach
--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -40,12 +40,27 @@
                 <!-- Product Image -->
                 <div class="col-lg-6">
                     <div class="details_image">
-                        <div class="details_image_large"><img src="/images/details_1.jpg" alt=""><div class="product_extra product_new"><a href="categories.html">New</a></div></div>
+                        @php
+                            $image = '';
+                            if(count($item->images) > 0){
+                                $image = $item->images[0]['img'];
+                            } else {
+                                $image = 'no_image.png';
+                            }
+                        @endphp
+                        <div class="details_image_large"><img src="/images/{{$image}}" alt="{{$item->title}}"><div class="product_extra product_new"><a href="categories.html">New</a></div></div>
                         <div class="details_image_thumbnails d-flex flex-row align-items-start justify-content-between">
-                            <div class="details_image_thumbnail active" data-image="/images/details_1.jpg"><img src="/images/details_1.jpg" alt=""></div>
-                            <div class="details_image_thumbnail" data-image="/images/details_2.jpg"><img src="/images/details_2.jpg" alt=""></div>
-                            <div class="details_image_thumbnail" data-image="/images/details_3.jpg"><img src="/images/details_3.jpg" alt=""></div>
-                            <div class="details_image_thumbnail" data-image="/images/details_4.jpg"><img src="/images/details_4.jpg" alt=""></div>
+                            @if($image == 'no_image.png')
+
+                            @else
+                                @foreach($item->images as $img)
+                                    @if($loop->first)
+                                        <div class="details_image_thumbnail active" data-image="/images/{{$img['img']}}"><img src="/images/{{$img['img']}}" alt="{{$item->title}}"></div>
+                                    @else
+                                        <div class="details_image_thumbnail" data-image="/images/{{$img['img']}}"><img src="/images/{{$img['img']}}" alt="{{$item->title}}"></div>
+                                    @endif
+                                @endforeach
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -54,16 +69,24 @@
                 <div class="col-lg-6">
                     <div class="details_content">
                         <div class="details_name">{{$item->title}}</div>
-                        <div class="details_discount">$890</div>
-                        <div class="details_price">$670</div>
+                        @if($item->new_price != null)
+                            <div class="details_discount">${{$item->price}}</div>
+                            <div class="details_price">${{$item->new_price}}</div>
+                        @else
+                            <div class="details_price">${{$item->price}}</div>
+                        @endif
 
                         <!-- In Stock -->
                         <div class="in_stock_container">
                             <div class="availability">Availability:</div>
-                            <span>In Stock</span>
+                            @if($item->in_stock)
+                                <span>In Stock</span>
+                            @else
+                                <span style="color: #cc0000">None</span>
+                            @endif
                         </div>
                         <div class="details_text">
-                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Phasellus id nisi quis justo tempus mollis sed et dui. In hac habitasse platea dictumst. Suspendisse ultrices mauris diam. Nullam sed aliquet elit. Mauris consequat nisi ut mauris efficitur lacinia.</p>
+                            <p>{{$item->description}}</p>
                         </div>
 
                         <!-- Product Quantity -->
@@ -100,7 +123,7 @@
                         <div class="reviews_title"><a href="#">Reviews <span>(1)</span></a></div>
                     </div>
                     <div class="description_text">
-                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Phasellus id nisi quis justo tempus mollis sed et dui. In hac habitasse platea dictumst. Suspendisse ultrices mauris diam. Nullam sed aliquet elit. Mauris consequat nisi ut mauris efficitur lacinia.</p>
+                        <p>{{$item->description}}</p>
                     </div>
                 </div>
             </div>
